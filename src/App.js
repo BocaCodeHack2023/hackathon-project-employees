3967a6f91b3bb20f95a15c7f3f294a9f9fe08001
Idan Fischer <Idanfischer5@gmail.com>
2023-09-30 12:02:24 -0400

    cleaned up code and started login

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './styles/App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        {/* <Route path='' element={<Login />}></Route> */}
+      </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
