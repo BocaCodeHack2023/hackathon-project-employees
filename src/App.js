693b1caba1c581542304be7bd48c0a19f01cf7e9
MarioD <mario@mario.com>
2023-09-30 19:35:32 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './styles/App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
+import "./styles/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path='' element={<Login />}></Route> */}
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
