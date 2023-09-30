eeee0c44823aec497930d50962f19bdd3080a1bd
Idan Fischer <Idanfischer5@gmail.com>
2023-09-30 18:11:20 -0400

    got appoimtement modal working and getting cards done

--- src/pages/AppointmentPicker.jsx
+++ src/pages/AppointmentPicker.jsx
@@ -1,13 +1,28 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Dropdown, Form, Row } from "react-bootstrap";
+import cancerTypes from "../utils/CancerTypes.json"
+import "../styles/appointmentPicker.css"
 
 export default function AppointmentPicker() {
 
+  // console.log(cancerTypes)
   return (
     <>
       <Container fluid>
         <Row>
-          <Col>
-            <h1>Appointment Picker</h1>
+          <Col sm="12">
+            <Card>
+              <Card.Body className="d-flex align-items-center justify-content-between">
+                <h1 className="font-size-app">
+                  Make A Pre-Screening Appointment
+                </h1>
+                <Form.Select className="width-responsive-app" aria-label="Type of Screening">
+                  {cancerTypes.map((cancer, index) => (
+                    <option key={index}>{cancer}</option>
+                  ))}
+                </Form.Select>
+
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
