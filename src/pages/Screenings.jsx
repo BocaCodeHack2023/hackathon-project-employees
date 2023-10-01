72b132861d702081623167ee9c24cf7e6ca4bd01
MarioD <mario@mario.com>
2023-10-01 02:16:55 -0400

    form

--- src/pages/Screenings.jsx
+++ src/pages/Screenings.jsx
@@ -7,18 +7,55 @@ import {
   Row,
   Button,
 } from "react-bootstrap";
-import { useState } from "react";
 import { mdiRocketLaunchOutline } from "@mdi/js";
 import Icon from "@mdi/react";
+import {
+  useState,
+  createContext,
+  useContext,
+  useReducer,
+  useEffect,
+} from "react";
+import { useNavigate } from "react-router-dom";
 
+import HTTP from "./../utils/http";
 import cancerTypes from "../utils/CancerTypes.json";
 import "../styles/screenings.css";
 import Sidebar from "../components/Sidebar";
 
+// id: string
+// user_id: string
+// location_id: string
+// screening_type: enum
+// date: date
+// status: enum[completed, pending, incomplete]
+// created_at: dob
+// updated_at: dob
+// notes: text
+// attachments
 export default function Screenings() {
-  const [showSidebar, setShowSidebar] = useState(false);
+  const [user, setUser] = useState({});
 
-  // console.log(cancerTypes)
+  const getUser = async (e) => {
+    try {
+      const response = await HTTP({
+        url: `/users/${process.env.REACT_APP_USER_ID}`,
+      });
+
+      console.log(response.data);
+      if (response?.data?.["_id"]) {
+        setUser(response.data);
+      }
+    } catch (error) {
+      console.log("ERROR: " + error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Use Effect");
+    getUser();
+  }, []);
+
   return (
     <>
       <Container className="mt-5">
@@ -37,31 +74,65 @@ export default function Screenings() {
             <Card>
               {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
+                <Card.Text></Card.Text>
+              </Card.Body>
 
-                <Form.Select
-                  className="width-responsive-app"
-                  aria-label="Type of Screening"
-                >
-                  {cancerTypes.map((cancer, index) => (
-                    <option key={index}>{cancer}</option>
-                  ))}
-                </Form.Select>
+              <Card.Body>
+                <Form>
+                  <Row>
+                    <Col md="6">
+                      <Form.Label>Patient Name</Form.Label>
 
-                <div className="d-grid gap-2">
-                  <Button variant="success" className="btn-block">
-                    <Icon
-                      path={mdiRocketLaunchOutline}
-                      size={1}
-                      color="white"
-                    />
-                    <span className="ml-5">Submit</span>
-                  </Button>
-                </div>
+                      <Form.Control
+                        value={`${user.name} ${user.last_name}`}
+                        disabled
+                      ></Form.Control>
+                    </Col>
+                    <Col md="6">
+                      <Form.Label>Email Address</Form.Label>
+
+                      <Form.Control value={user.email} disabled></Form.Control>
+                    </Col>
+
+                    <Col md="6" className="mt-3">
+                      <Form.Label>Email address</Form.Label>
+
+                      <Form.Select aria-label="Type of Screening">
+                        {cancerTypes.map((cancer, index) => (
+                          <option key={index}>{cancer}</option>
+                        ))}
+                      </Form.Select>
+                    </Col>
+                    <Col md="6" className="mt-3">
+                      <Form.Label>Date</Form.Label>
+
+                      <Form.Select aria-label="Type of Screening">
+                        {cancerTypes.map((cancer, index) => (
+                          <option key={index}>{cancer}</option>
+                        ))}
+                      </Form.Select>
+                    </Col>
+                  </Row>
+
+                  <Form.Group className="mt-3" controlId="formBasicEmail">
+                    <Form.Label>Notes</Form.Label>
+                    <Form.Control as="textarea" style={{ height: "100px" }} />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
+
+                  <div className="d-grid gap-2 mt-3">
+                    <Button variant="success" className="btn-block">
+                      <Icon
+                        path={mdiRocketLaunchOutline}
+                        size={1}
+                        color="white"
+                      />
+                      <span className="ml-5">Submit</span>
+                    </Button>
+                  </div>
+                </Form>
               </Card.Body>
             </Card>
           </Col>
